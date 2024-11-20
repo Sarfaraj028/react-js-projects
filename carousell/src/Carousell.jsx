6c236c7f4598dab16f0363ea20a14f1a3396eed1
Sarfaraj028 <sarfaraja0869@gmail.com>
2024-11-20 17:22:57 +0530

    Many changes

--- carousell/src/Carousell.jsx
+++ carousell/src/Carousell.jsx
@@ -10,7 +10,7 @@ const images = [
 ];
 
 function Carousell() {
-    const [current, setCurrent] =useState(0);
+    const [current, setCurrent] = useState(0);
 
     const nextSlide = () => {
         setCurrent(current === images.length - 1 ? 0 : current + 1)
@@ -21,16 +21,17 @@ function Carousell() {
     
   return (
     <div className='center'>
-        <div className='prev' onClick={prevSlide}><i class='bx bx-left-arrow-alt'></i></div>
+        <div className='prev' onClick={prevSlide}><i className='bx bx-left-arrow-alt'></i></div>
         <div className='images'>
             {/* {images.map((image, index) =>
                 current === index && 
                 <img src={image} key={index} alt='image'/>
-            )}  instead of these */}
+            )}  instead of these 
+            */}
 
             {/* just write this bcz we are not rendering all images we're just displaying one  */}
                 
-            <img src={images[current]} alt="carousell" />
+            <img  src={images[current]} alt="carousell" />
                    
         </div>
         <div className='next' onClick={nextSlide}><i class='bx bx-right-arrow-alt' ></i></div>
