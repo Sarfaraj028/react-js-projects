54d7a59bc7bf21c83ab75aa56fa134f1fb907891
Sarfaraj028 <sarfaraja0869@gmail.com>
2024-12-01 13:59:19 +0530

    more functionality adde to the project

--- word-char-counter/src/WordCharCounter.jsx
+++ word-char-counter/src/WordCharCounter.jsx
@@ -1,21 +1,35 @@
 import React, { useState } from 'react'
 import './style.css';
 function WordCharCounter() {
-    const [text, setText] = useState("")
+    const [text, setText] = useState("message");
 
     const handleChange = (e) =>{
         setText(e.target.value)
     }
+
+    // handle clear text
+    const handleClear = () =>{
+      setText("")
+    }
+
+    //  handle upperCase
+    const toUpper = () =>{
+      setText(text.toUpperCase())
+    }
+
   return (
-    <div>
+    <div className='center'>
         <h2>Enter your text here........</h2>
-        <textarea rows={10} placeholder='type your message here....' cols={70} value={text} onChange={handleChange}/>
+        <textarea rows={10}  cols={70} value={text} onChange={handleChange}/>
         <p>
           {text.split(" ").filter(Boolean).length}  words  {text.length} characters 
+          <span onClick={toUpper} >to UppeCase</span>
+          <span onClick={handleClear} >Clear Text</span>
+          
         </p>
 
         <h2>Preview: </h2>
-        <p>{text}</p>
+        <p style={{fontSize: "15px"}}>{text}</p>
     </div>
   )
 }
